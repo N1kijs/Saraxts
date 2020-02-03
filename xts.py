540bdb43a2a8a834c37a7b9ff6f44e59c4616129
GvidoJ <gvidojakobsons46@gmail.com>
2020-02-03 13:50:55 +0200

    a

--- xts.py
+++ xts.py
@@ -3,6 +3,5 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/hello/')
-@app.route('/hello?<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+def hello():
+    return render_template('hello.html')
