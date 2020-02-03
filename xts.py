6142011c27dbc0afda13ef65d974384ef9d8be9b
GvidoJ <gvidojakobsons46@gmail.com>
2020-02-03 14:03:36 +0200

    Update xts.py

--- xts.py
+++ xts.py
@@ -1,7 +1,16 @@
+import pandas as pd
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-@app.route('/hello/')
-def hello():
-    return render_template('hello.html')
+@app.route('/')
+def jeff():
+    df = pr.read_excel('omegalul.xlsx')
+    return df.to_html()
+
+# @app.route('/hello')
+# def hello():
+#     return render_template('hello.html')
+
+if __name__ == '__main__'
+    app.run()
