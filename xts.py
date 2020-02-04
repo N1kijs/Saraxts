c1abb95fff0c6c743f1436db07a1e5bf79779e45
GvidoJ <gvidojakobsons46@gmail.com>
2020-02-04 09:14:59 +0200

    Update xts.py

--- xts.py
+++ xts.py
@@ -8,9 +8,9 @@ def hello():
     df = pd.read_excel('C:\\Users\\gvido\\Desktop\\Projektu ned\\omegalul.xlsx')
     return df.to_html()
 
-# @app.route('/work')
-# def test():
-#     return render_template('hello.html')
+@app.route('/work')
+def test():
+    return render_template('hello.html')
 
 if __name__ == '__main__':
     app.run()
