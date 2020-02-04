3ff8d5b3312286b3bcf5719657ab4b89364d4ddb
GvidoJ <gvidojakobsons46@gmail.com>
2020-02-04 11:06:46 +0200

    s

--- xts.py
+++ xts.py
@@ -1,11 +1,15 @@
 import pandas as pd
 from flask import Flask, render_template
+from openpyxl import load_workbook
+workbook = load_workbook(filename='omegalul.xlsx')
 app = Flask(__name__)
 
 @app.route('/sheet')
 def hello():
-    df = pd.read_excel("sheets/10-12kl_27_ I.xlsx")
+    needs = sheet['B3'].value
+    df = needs
     return df.to_html()
+
 
 @app.route('/work')
 def test():
