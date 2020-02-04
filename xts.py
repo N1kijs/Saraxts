a52bb28742b95bb8c674eb2bfb2b54af9e7fce55
GvidoJ <gvidojakobsons46@gmail.com>
2020-02-04 11:29:35 +0200

    Update xts.py

--- xts.py
+++ xts.py
@@ -1,14 +1,19 @@
 import pandas as pd
 from flask import Flask, render_template
-from openpyxl import load_workbook
+from openpyxl import Workbook, load_workbook
 workbook = load_workbook(filename='sheets/omegalul.xlsx')
 sheet = workbook.active
+
+sheet['B3']
+
+workbook.save(filename='need.xlsx')
+
 app = Flask(__name__)
 
 @app.route('/sheet')
 def hello():
     
-    df = pd.read_excel(sheet['B3'])
+    df = pd.read_excel('need.xlsx')
     return df.to_html()
 
 
