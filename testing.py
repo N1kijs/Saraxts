fb4e4a587d7f6dccf91cc3b366a78301dacd71b5
GvidoJ <gvidojakobsons46@gmail.com>
2020-02-05 09:53:11 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -5,13 +5,17 @@ import schedule
 memes = 1
 
 def gay():
+    global memes
     memes += 1
     print(memes)
 
 def gaynt():
+    global memes
     memes == 0
+    print(memes)
     
-schedule.every().day.at("09:49").do(gay) 
+schedule.every().wednesday.at("09:54").do(gay)
+schedule.every().wednesday.at("09:55").do(gaynt) 
 
 while True:
     schedule.run_pending()
