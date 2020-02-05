d4e600d1e1ee945da757e351b3d2e3ec334a04c0
GvidoJ <gvidojakobsons46@gmail.com>
2020-02-05 12:14:07 +0200

    Update sched.py

--- sched.py
+++ sched.py
@@ -1,11 +1,6 @@
 import schedule
 import time
 
-start_time = time.time()
-
-def job():
-    print("I'm working..." + str(time.time() - start_time))
-
 def run_schedule():
     while True:
         schedule.run_pending()
