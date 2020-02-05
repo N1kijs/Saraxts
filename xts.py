5fe6a889ca20faf01fa4c5692849cdb980f0db19
GvidoJ <gvidojakobsons46@gmail.com>
2020-02-05 13:29:38 +0200

    Update xts.py

--- xts.py
+++ xts.py
@@ -216,8 +216,7 @@ def hello():
         return render_template('hello.html')
     elif stundas >= 1:
         df = pd.read_excel('need.xlsx')
-        return render_template('hello.html')
-        return df.to_html()
+        return render_template('hello.html'), df.to_html()
 
 if __name__ == '__main__':
     app.run()
